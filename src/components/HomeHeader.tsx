ca97a4a1c00f2b3c466ad5b36a853a490b320e42
Lucas Gabriel <gabif.johann@gmail.com>
2024-01-10 23:48:09 -0300

    add: Home Header component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,34 @@
+import { TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { HStack, Heading, Icon, Text, VStack } from 'native-base'
+
+import { UserPhoto } from './UserPhoto'
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{
+          uri: 'http://github.com/gabjohann.png',
+        }}
+        alt="Imagem de perfil do usuário"
+        mr={4}
+        size={16}
+      />
+
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100" fontSize="md">
+          Lucas
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
